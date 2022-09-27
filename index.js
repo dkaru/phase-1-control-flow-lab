07d566c4285becb964769186f5ff151caa29a220
Moringa School <dwangui799@gmail.com>
2022-09-27 12:18:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet)
+  if (feet<= 400){
+   return  "This one is for me!";
+  }else if (1900 < feet && feet < 2500) {
+    return "I will gladlytake your thirty bucks,"
+  }else {
+    return "No can do."
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ function ternaryCheckCity (city) {
+// write your code here! 
+  return city === 'NYC' ? "oK, sounds good." : "No go."
+ }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+ function  sWitchOnCharnFromTip (tip){
+// write your code here!
+ switch (tip) {
+  case 'generous':
+  return 'Thank you so much.';
+  case 'not as generous.':
+    return 'Thank you.' ;
+    default:
+      return 'Bye.'
+ }
+
+ 
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
